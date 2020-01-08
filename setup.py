7cfe5a25376c0cfa9471dcff7e966c20b9bed32a
Joseph Kogut <joseph.kogut@gmail.com>
2020-01-08 15:22:10 -0800

    setup: add build_subprocess to commands

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+from distutils.core import Command
+from distutils.command.build import build
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
 
 from Cython.Build import cythonize
 
 import os
+import subprocess
+
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 include_dirs=[
@@ -28,6 +32,39 @@ package_data={
     'cefcython.util': ['*.pxd'],
 }
 
+class BuildSubprocessCommand(Command):
+    def initialize_options(self):
+        self.build_lib = None
+        self.build_temp = None
+        self.force = None
+
+    def finalize_options(self):
+        self.set_undefined_options('build',
+                                   ('build_lib', 'build_lib'),
+                                   ('build_temp', 'build_temp'),
+                                   ('force', 'force'))
+
+    def copy_src(self):
+        self.copy_tree('src', os.path.join(self.build_temp, 'src'))
+
+    def build(self):
+        subprocess_dir = os.path.join(self.build_temp, os.path.join('src', 'subprocess'))
+        cmd = ['make', '-C', subprocess_dir, 'CEF_INCLUDE={}'.format(ROOT_DIR)]
+        if self.force:
+            cmd += ['clean', 'all']
+        subprocess.check_call(cmd)
+        self.copy_file(os.path.join(subprocess_dir, 'subprocess'),
+                       os.path.join(self.build_lib, os.path.join('cefcython', 'subprocess')))
+
+    def run(self):
+        self.copy_src()
+        self.build()
+
+class BuildCommand(build):
+    def run(self):
+        build.run(self)
+        self.run_command('build_subprocess')
+
 from cefcython import __version__ as version
 
 setup(
@@ -41,4 +78,8 @@ setup(
     package_data=package_data,
     ext_modules=cythonize(extensions),
     zip_safe=False,
+    cmdclass={
+        'build': BuildCommand,
+        'build_subprocess': BuildSubprocessCommand,
+    }
 )
