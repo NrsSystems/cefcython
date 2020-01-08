6ac336db9124deb09982ab0c98b8a7b96f43180d
Joseph Kogut <joseph.kogut@gmail.com>
2020-01-08 15:16:15 -0800

    setup: simplify extension definitions

--- setup.py
+++ setup.py
@@ -12,27 +12,14 @@ include_dirs=[
 
 extensions = [
     Extension(
-        'capi.cef_app',
-        ['capi/cef_app.pyx'],
+        'cefcython.capi.*',
+        ['cefcython/capi/*.pyx'],
         include_dirs=include_dirs,
     ),
     Extension(
-        'capi.cef_client',
-        ['capi/cef_client.pyx'],
+        'cefcython.util.*',
+        ['cefcython/util/*.pyx'],
         include_dirs=include_dirs,
-    ),
-    Extension(
-        'capi.cef_life_span_handler',
-        ['capi/cef_life_span_handler.pyx'],
-        include_dirs=include_dirs,
-    ),
-    Extension(
-        'example.app',
-        ['example/app.pyx'],
-        include_dirs=include_dirs,
-        library_dirs=['/home/joseph/cef-cython-exp/cef_binary_79.0.10+ge866a07+chromium-79.0.3945.88_linux64/Release',
-                      '/home/joseph/cef-cython-exp/cef_binary_79.0.10+ge866a07+chromium-79.0.3945.88_linux64/build/libcef_dll_wrapper'],
-        libraries=['cef_dll_wrapper', 'cef'],
     ),
 ]
 
