b6ab224ecd5f5b24dd903184b1f290ca123e8a0f
Joseph Kogut <joseph.kogut@gmail.com>
2020-01-15 12:09:44 -0800

    implement basic refcounting

--- cefcython/capi/cef_base.pxd
+++ cefcython/capi/cef_base.pxd
@@ -4,12 +4,12 @@ cdef extern from "include/capi/cef_base_capi.h":
     cdef struct _cef_base_ref_counted_t:
         size_t size
 
-        void (* add_ref)(_cef_base_ref_counted_t *self)
-        int (* release)(_cef_base_ref_counted_t *self)
-        int (* has_one_ref)(_cef_base_ref_counted_t *self)
-        int (* has_at_least_one_ref)(_cef_base_ref_counted_t *self)
+        void (* add_ref)(_cef_base_ref_counted_t *self) with gil
+        int (* release)(_cef_base_ref_counted_t *self) with gil
+        int (* has_one_ref)(_cef_base_ref_counted_t *self) with gil
+        int (* has_at_least_one_ref)(_cef_base_ref_counted_t *self) with gil
 
     ctypedef _cef_base_ref_counted_t cef_base_ref_counted_t
 
-cdef void initialize_cef_base_ref_counted(cef_base_ref_counted_t *base)
+cdef void cef_base_ref_counted_init(cef_base_ref_counted_t *base)
 
--- cefcython/capi/cef_base.pyx
+++ cefcython/capi/cef_base.pyx
@@ -1,33 +1,82 @@
 from libc.stddef cimport size_t
+from libc.stdint cimport uintptr_t
+
+from cefcython.capi cimport (cef_browser_t,
+                             cef_app_t,
+                             cef_client_t)
 
 DEF DEBUG_REFERENCE_COUNTING = 0
 
-cdef void DEBUG_CALLBACK(char *msg):
-    printf(msg)
+cdef dict _refcounts = {}
 
-cdef void add_ref(cef_base_ref_counted_t *self):
-    DEBUG_CALLBACK("cef_base_ref_counted_t.add_ref\n")
-    if DEBUG_REFERENCE_COUNTING:
-        printf("+")
+cdef size_t sizeof_cef_app_t = sizeof(cef_app_t)
+cdef size_t sizeof_cef_base_ref_counted_t = sizeof(cef_base_ref_counted_t)
+cdef size_t sizeof_cef_browser_t = sizeof(cef_browser_t)
+cdef size_t sizeof_cef_client_t = sizeof(cef_client_t)
+#cdef size_t sizeof_cef_v8handler_t = sizeof(cef_v8_handler_t)
+#cdef size_t sizeof_cef_v8value_t = sizeof(cef_v8value_t)
 
-cdef int release(cef_base_ref_counted_t *self):
-    DEBUG_CALLBACK("cef_base_ref_counted_t.release\n")
-    if DEBUG_REFERENCE_COUNTING:
-        printf("-")
-    return 1
+cdef const char *_get_base_type_str(cef_base_ref_counted_t *self) with gil:
+    if not self:
+        return "UNKNOWN"
+    elif self.size == sizeof_cef_app_t:
+        return 'cef_app_t'
+    elif self.size == sizeof_cef_base_ref_counted_t:
+        return 'cef_base_ref_counted_t'
+    elif self.size == sizeof_cef_browser_t:
+        return 'cef_browser_t'
+    elif self.size == sizeof_cef_client_t:
+        return 'cef_client_t'
 
-cdef int has_one_ref(cef_base_ref_counted_t *self):
-    DEBUG_CALLBACK("cef_bae_ref_counted_t.has_one_ref\n")
-    if DEBUG_REFERENCE_COUNTING:
-        printf("=")
-    return 1
+cdef void add_ref(cef_base_ref_counted_t *self) with gil:
+    global _refcounts
+    cdef uintptr_t base = <uintptr_t>self
+    printf('[%s(0x%08lx)] add_ref\n', _get_base_type_str(self), base)
+    if base not in _refcounts:
+        _refcounts[base] = 1
+    else:
+        _refcounts[base] += 1
+    cdef int refcount = _refcounts[base]
+    printf('[%s(0x%08lx)] added_ref: %i\n', _get_base_type_str(self), base, refcount)
 
-cdef void initialize_cef_base_ref_counted(cef_base_ref_counted_t *base):
-    cdef size_t size = base.size
-    if size <= 0:
+cdef int release(cef_base_ref_counted_t *self) with gil:
+    global _refcounts
+    cdef uintptr_t base = <uintptr_t>self
+    printf('[%s(0x%08lx)] release\n', _get_base_type_str(self), base)
+    if not base in _refcounts:
+        printf('[%s(0x%08lx)] released: False\n', _get_base_type_str(self), base)
+        return False
+
+    cdef int refcount
+    if not _refcounts[base] - 1:
+        _refcounts.pop(base)
+        printf('[%s(0x%08lx)] released: %i, True\n', _get_base_type_str(self), base, 0)
+        return True
+    else:
+        _refcounts[base] -=1
+        refcount = _refcounts[base]
+        printf('[%s(0x%08lx)] released: %i, False\n', _get_base_type_str(self), base, refcount)
+        return False
+
+cdef int has_one_ref(cef_base_ref_counted_t *self) with gil:
+    cdef uintptr_t base = <uintptr_t>self
+    cdef int ret = (_refcounts.get(base, 0) == 1)
+    printf('[%s(0x%08lx)] has_one_ref: %i\n', _get_base_type_str(self), base, ret)
+    return ret
+
+cdef int has_at_least_one_ref(cef_base_ref_counted_t *self) with gil:
+    cdef uintptr_t base = <uintptr_t>self
+    cdef int ret = (_refcounts.get(base, 0) >= 1)
+    printf('[%s(0x%08lx)] has_at_least_one_ref: %i\n', _get_base_type_str(self), base, ret)
+    return ret
+
+cdef void cef_base_ref_counted_init(cef_base_ref_counted_t *base):
+    if base.size <= 0:
         printf("FATAL: initiailize_cef_base failed, size member not set\n")
         exit(1)
 
+    printf('[%s(0x%08lx)] init_cef_base\n', _get_base_type_str(base), base)
     base.add_ref = add_ref
     base.release = release
     base.has_one_ref = has_one_ref
+    base.has_at_least_one_ref = has_at_least_one_ref
