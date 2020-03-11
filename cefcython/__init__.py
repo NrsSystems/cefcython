1573fa6e780b29cc7259917a672e312999657e7e
Joseph Kogut <joseph.kogut@gmail.com>
2020-03-11 16:31:25 -0700

    add path/flag utils

--- cefcython/__init__.py
+++ cefcython/__init__.py
@@ -1,1 +1,26 @@
 __version__ = '0.1'
+
+import os
+
+PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+def get_module_dir():
+    return os.path.join(PACKAGE_DIR, 'cef')
+
+def get_include_dirs():
+    return [os.path.join(PACKAGE_DIR, 'build')]
+
+def get_lib_dirs():
+    return [get_module_dir()]
+
+def get_libs():
+    return ['cef', 'X11']
+
+def get_cflags():
+    return ' -I'.join([''] + get_include_dirs())
+
+def get_ldflags():
+    return ' -L'.join([''] + get_lib_dirs()) + ' -l'.join([''] + get_libs())
+
+def get_lflags():
+    return ' -Wl,-rpath,{}'.format(os.path.join(PACKAGE_DIR, 'cef'))
