7b66ec9e4ce2431962b27a3ed9835e485b32003b
Joseph Kogut <joseph.kogut@gmail.com>
2020-10-07 15:54:31 -0700

    separate build and runtime package dirs

--- cefcython/__init__.py
+++ cefcython/__init__.py
@@ -1,12 +1,20 @@
 import os
 
-PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
+BUILD_PACKAGE_DIR = os.environ.get(
+    'CEFCYTHON_BUILD_PACKAGE_DIR',
+    os.path.abspath(os.path.dirname(__file__))
+)
+
+RUNTIME_PACKAGE_DIR = os.environ.get(
+    'CEFCYTHON_RUNTIME_PACKAGE_DIR',
+    BUILD_PACKAGE_DIR,
+)
 
 def get_module_dir():
-    return os.path.join(PACKAGE_DIR, 'cef')
+    return os.path.join(RUNTIME_PACKAGE_DIR, 'cef')
 
 def get_include_dirs():
-    return [os.path.join(PACKAGE_DIR, 'build')]
+    return [os.path.join(BUILD_PACKAGE_DIR, 'build')]
 
 def get_lib_dirs():
     return [get_module_dir()]
@@ -21,4 +29,4 @@ def get_ldflags():
     return ' -L'.join([''] + get_lib_dirs()) + ' -l'.join([''] + get_libs())
 
 def get_lflags():
-    return ' -Wl,-rpath,{}'.format(os.path.join(PACKAGE_DIR, 'cef'))
+    return ' -Wl,-rpath,{}'.format(os.path.join(RUNTIME_PACKAGE_DIR, 'cef'))
